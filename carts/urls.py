cc644c34d4c859c07901d044206a61c13661be42
Bogdan <bogdanvasilev225@gmail.com>
2024-03-23 17:34:33 +0400

    add to cart view refactor included cart templates

--- carts/urls.py
+++ carts/urls.py
@@ -5,9 +5,9 @@ from carts import views
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart_add/<int:product_id>/', views.cart_add, name='cart_add'),
-    path('cart_chande/<int:product_id>/', views.cart_chande, name='cart_chande'),
-    path('cart_remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
+    path('cart_chande/<slug:product_slug>/', views.cart_chande, name='cart_chande'),
+    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'),
 ]
 
 
